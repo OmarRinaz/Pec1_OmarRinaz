7cb5f0dfd073aea757e714ba6fde42e47115438d
pikipoka <pikipoka@gmail.com>
2016-11-04 17:34:26 +0100

    First push

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	#region private vars
+	#endregion
+	#region public vars
+	#endregion
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	#region Game Application Managment METHODS
+
+	// Use this for exit / usar esto para salir
+	public void ExitGame(){		
+		Debug.Log ("Closing Application / Cerrando Aplicación");
+		if (UnityEditor.EditorApplication.isPlaying) UnityEditor.EditorApplication.isPlaying = false; //use this for editor / usar esto para el editor
+		Application.Quit ();
+	}
+
+	#endregion
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
