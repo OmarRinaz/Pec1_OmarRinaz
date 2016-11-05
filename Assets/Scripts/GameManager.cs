f9460d0a6f681f190374c62585df59c3f832095c
pikipoka <pikipoka@gmail.com>
2016-11-05 18:46:41 +0100

    Reorganitzation , SceneManager, First version of UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,29 +1,55 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
-	#region private vars
-	#endregion
+
 	#region public vars
+	public static GameManager Instance {
+		get {
+			if (instance == null)
+				Debug.Log ("Error, GameManager does not exist. ¡Attach GameManager Script to a game object named GameManager!");
+			return instance;
+		} 
+	}
 	#endregion
 
+	#region private vars
+	private static GameManager instance = null;
+	#endregion
+
+	#region Unity Methods
+	void Awake(){
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (this.gameObject);
+		DontDestroyOnLoad (this.gameObject);
+	}
 	// Use this for initialization
 	void Start () {
 	
 	}
-	#region Game Application Managment METHODS
+	// Update is called once per frame
+	void Update () {
 
+	}
+	#endregion
+
+	#region Game Application Managment METHODS
 	// Use this for exit / usar esto para salir
 	public void ExitGame(){		
 		Debug.Log ("Closing Application / Cerrando Aplicación");
 		if (UnityEditor.EditorApplication.isPlaying) UnityEditor.EditorApplication.isPlaying = false; //use this for editor / usar esto para el editor
 		Application.Quit ();
 	}
-
-	#endregion
-
-	// Update is called once per frame
-	void Update () {
-	
+	// Use this to start the game / usar esto para empezar el juego
+	public void NewGame(){
+		SceneManager.LoadScene ("NewGameScene");
 	}
+	// Use this to restart the current scene / usar esto para reiniciar la escena actual
+	public void RestartScene(){
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+	}
+	#endregion
 }
